7bbd6217b2237621d298b0c8e08feb6a2a181b51
welovecherry <jungmin_hong@icloud.com>
2025-04-23 18:49:01 +0900

    fix: added 30 loop with if statement

--- fd.py
+++ fd.py
@@ -1,5 +1,14 @@
 # print "Fizz" if multiples of 3
 
-	elif i % 3 == 0:
-		print("Fizz")
+def fizzbuzz(n):
+	if n % 3 == 0 and n % 5 == 0:
+		return "FizzBuzz"
+	elif n % 3 == 0:
+		return "Fizz"
+	elif n % 5 == 0:
+		return "Buzz"
+	else:
+		return str(n)
 
+for i in range(1, 31):
+	print(fizzbuzz(i))
