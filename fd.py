44e13ddd7010cd052e59ca65f1f58b8c90321a4c
LearnSphere-2025 <workshopai02024@gmail.com>
2025-04-24 09:26:33 +0900

    feat : print buzz

--- fd.py
+++ fd.py
@@ -1,8 +1,6 @@
-# Multiple 15 and 5
+# Multiple 5
 for i in range(1, 20+1):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 5 ==0:
+    if i % 5 == 0:
         print('buzz')
     else:
         print(f'{i}')
